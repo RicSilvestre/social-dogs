1eacca2137a78e80e71f011cb00c621cb6ea2baf
Ricardo Silvestre <ricardomsilvestre@outlook.com>
2020-12-19 01:30:14 -0300

    Login 1

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header'
 import Home from './Components/Home'
 import Footer from './Components/Footer'
-import Login from './Components/Login'
+import Login from './Components/Login/Login'
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
         <Header/>
         <Routes>
           <Route path="/" element={<Home/>}/>
-          <Route path="/login" element={<Login/>}/>
+          <Route path="/login/*" element={<Login/>}/>
         </Routes>
         <Footer/>
       </BrowserRouter>
