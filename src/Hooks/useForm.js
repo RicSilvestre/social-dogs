bc4e01fb8b734f5d7a552ff4b98071387fb4ad60
Ricardo Silvestre <ricardomsilvestre@outlook.com>
2020-12-25 01:50:59 -0300

    Início modal

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -8,6 +8,10 @@ const types = {
     password: {
         regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
         message: "A senha precisa ter 1 caracter maiúsculo, 1 minúsculo e 1 dígito. Com no mínimo oito caracteres"
+    },
+    number: {
+        regex: /^\d+$/,
+        message: "Utilize números apenas"
     }
 }
 const useForm = (type) => {
